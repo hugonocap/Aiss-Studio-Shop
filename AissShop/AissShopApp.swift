288efc39f165e253f1fab3434bbe35d3e53ee88a
Hugo Montana <benzlbenny2k@gmail.com>
2022-10-22 12:14:53 +0300

    Initial Commit

---
+++ AissShop/AissShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  AissShopApp.swift
+//  AissShop
+//
+//  Created by Hugo on 22.10.22.
+//
+
+import SwiftUI
+
+@main
+struct AissShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
